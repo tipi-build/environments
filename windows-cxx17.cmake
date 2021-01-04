da8926efe028c884770cc9ec0de97dae76c50a63
Lambourl <65184664+Lambourl@users.noreply.github.com>
2021-01-04 09:18:58 +0100

    :wink: add file for clang on macos

--- windows-cxx17.cmake
+++ windows-cxx17.cmake
@@ -19,13 +19,3 @@ include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
-
-if (CMAKE_HOST_APPLE)
-  # Needed for boost to actually build  
-  execute_process (
-    COMMAND bash -c "xcrun --sdk macosx --show-sdk-path"
-    OUTPUT_VARIABLE SDKROOT_PATH
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(ENV{SDKROOT} "${SDKROOT_PATH}")
-  set(CMAKE_SYSROOT "${SDKROOT_PATH}" CACHE PATH "")
-endif()
