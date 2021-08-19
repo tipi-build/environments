5cb77b018cf3577d5992271a73570758ae732eb0
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2021-08-19 22:38:04 +0200

    :wink: ssh enabled docker image.

--- wasm.pkr.js
+++ wasm.pkr.js
@@ -3,11 +3,21 @@
   "builders": [
     {
       "type": "docker",
-      "image": "nxxm/tipi-ubuntu-staging-275",
+      "image": "tipibuild/tipi-ubuntu",
       "commit": true,
       "changes": [
+        "RUN apt install openssh-server",
+        "RUN service ssh start"
         "EXPOSE 22"
       ]
     }
+  ],
+  "post-processors": [
+    { 
+      "type": "docker-tag",
+      "repository": "wasm",
+      "tag": "latest"
+    }
   ]
+
 }
