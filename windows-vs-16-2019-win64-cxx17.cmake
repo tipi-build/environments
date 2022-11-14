a4f9a123fb9b1a942039dc56ca252f923f437ece
Lambourl <lmbrluc@gmail.com>
2022-11-14 10:39:23 +0100

    :wrench: dont copy paste use include

--- windows-vs-16-2019-win64-cxx17.cmake
+++ windows-vs-16-2019-win64-cxx17.cmake
@@ -1,18 +1,1 @@
-# Copyright (c) 2015-2017, Ruslan Baratov
-# All rights reserved.
-
-if(DEFINED POLLY_VS_16_2019_WIN64_CXX17_CMAKE_)
-  return()
-else()
-  set(POLLY_VS_16_2019_WIN64_CXX17_CMAKE_ 1)
-endif()
-
-include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
-
-polly_init(
-    "Visual Studio 16 2019 Win64 / C++17"
-    "Visual Studio 16 2019"
-)
-
-include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/flags/vs-cxx17.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/vs-16-2019-win64-cxx17.cmake")
--- windows-vs-16-2019-win64.cmake
+++ windows-vs-16-2019-win64.cmake
@@ -1,17 +1,1 @@
-# Copyright (c) 2015-2017, Ruslan Baratov
-# All rights reserved.
-
-if(DEFINED POLLY_VS_16_2019_WIN64_CMAKE_)
-  return()
-else()
-  set(POLLY_VS_16_2019_WIN64_CMAKE_ 1)
-endif()
-
-include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
-
-polly_init(
-    "Visual Studio 16 2019"
-    "Visual Studio 16 2019"
-)
-
-include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/vs-16-2019-win64-cxx17.cmake")
