7fce23fe4ac044c90ed672f21cf57e840cce7ee7
Lambourl <lmbrluc@gmail.com>
2023-07-07 16:09:53 +0200

    :wink: add toolchain to support wine msvc

---
+++ linux-wine-msvc.pkr.js
@@ -0,0 +1,19 @@
+{
+  "variables": { },
+  "builders": [
+    {
+      "type": "docker",
+      "image": "tipibuild/tipi-ubuntu-wine-msvc:{{tipi_cli_version}}",
+      "commit": true
+    }
+  ],
+  "post-processors": [
+    { 
+      "type": "docker-tag",
+      "repository": "linux",
+      "tag": "latest"
+    }
+  ]
+  ,"_tipi_version":"{{tipi_version_hash}}"
+
+}
