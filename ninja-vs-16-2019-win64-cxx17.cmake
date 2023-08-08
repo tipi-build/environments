a11c20665bf716c214ca156807e8ed84e3db021b
Lambourl <lmbrluc@gmail.com>
2023-08-08 19:10:14 +0200

    :wrench: we need to keep msbuild

--- ninja-vs-16-2019-win64-cxx17.cmake
+++ ninja-vs-16-2019-win64-cxx17.cmake
@@ -1,4 +1,5 @@
 # Copyright (c) 2015-2017, Ruslan Baratov
+# Copyright (c) 2015-2017, Ruslan Baratov
 # All rights reserved.
 
 if(DEFINED POLLY_VS_16_2019_WIN64_CXX17_CMAKE_)
@@ -11,8 +12,9 @@ include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
 polly_init(
     "Visual Studio 16 2019 Win64 / C++17"
-    "Visual Studio 16 2019"
+    "Ninja"
 )
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/cl.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/vs-cxx17.cmake")
--- ninja-vs-17-2022-win64-cxx17.cmake
+++ ninja-vs-17-2022-win64-cxx17.cmake
@@ -1,4 +1,5 @@
 # Copyright (c) 2015-2017, Ruslan Baratov
+# Copyright (c) 2015-2017, Ruslan Baratov
 # All rights reserved.
 
 if(DEFINED POLLY_VS_16_2019_WIN64_CXX17_CMAKE_)
@@ -10,9 +11,10 @@ endif()
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
 polly_init(
-    "Visual Studio 16 2019 Win64 / C++17"
-    "Visual Studio 16 2019"
+    "Visual Studio 19 2022 Win64 / C++17"
+    "Ninja"
 )
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/cl.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/vs-cxx17.cmake")
