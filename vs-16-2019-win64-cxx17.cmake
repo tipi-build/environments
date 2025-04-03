fbe0451919fe79314af65313ac9ec50a64431263
Damien Buhl (alias daminetreg) <damien@tipi.build>
2025-04-03 09:19:22 +0200

    :gear: add defaults to support properly std::min,max, winsock and Lyra on Windows

--- vs-16-2019-win64-cxx17.cmake
+++ vs-16-2019-win64-cxx17.cmake
@@ -16,3 +16,14 @@ polly_init(
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/vs-cxx17.cmake")
+
+add_compile_definitions(
+    NOMINMAX
+    WIN32_LEAN_AND_MEAN
+    _WIN32_WINNT=0x0A00 # We have to set the windows version targeted
+    WINVER=0x0A00 # We have to set the windows version targeted
+    LYRA_CONFIG_OPTIONAL_TYPE=std::optional
+
+)
+
+set (CMAKE_SYSTEM_VERSION "10.0.22621.0" CACHE STRING "Ensure WinSDK is recent enough to compile in C++17" FORCE )
