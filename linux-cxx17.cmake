dd462ef9c6f3c446eba7b34783600c90f7c88d8b
Damien Buhl (alias daminetreg) <damien@tipi.build>
2025-04-16 11:58:19 +0200

    :gear: linux-cxx17 defauls to fully static builds with libc++.

--- linux-cxx17.cmake
+++ linux-cxx17.cmake
@@ -20,6 +20,9 @@ include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
 
 set(CMAKE_CXX_FLAGS " ${CMAKE_CXX_FLAGS} -stdlib=libc++ ")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=lld")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=lld -stdlib=libc++ -static-libstdc++ -static-libgcc /usr/local/share/.tipi/clang/4f846ee/lib/libc++.a /usr/local/share/.tipi/clang/4f846ee/lib/libc++abi.a")
+# Required flags for -stdlib=libc++ are provided in https://github.com/tipi-build/environments/tree/feature/tipi-bootstrap loaded by .tipi/distro.json via .tipi/env
+# Thes are in the main distro and not in an opts.toolchain files because for now the cmake-tipi-provider cannot use the opts.toolchain files and this would load a broken
+# cpp-pre not using the same flags that is depended by elfshaker CMakeLists.
 
 set (CMAKE_POSITION_INDEPENDENT_CODE ON)
