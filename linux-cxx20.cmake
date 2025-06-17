6fbefcf322ed4b3ead1a4aca212b45a7c587268e
Damien Buhl (alias daminetreg) <damien@tipi.build>
2025-06-17 13:36:35 +0200

    :gear: backport official tipi environment that are officially available as of 6ae26a92961c0354468a10d849c85d4d474ddfe2 in feature/tipi.

--- linux-cxx20.cmake
+++ linux-cxx20.cmake
@@ -2,19 +2,19 @@
 # Copyright (c) 2017, David Hirvonen
 # All rights reserved.
 
-if(DEFINED POLLY_CLANG_CXX17_CMAKE_)
+if(DEFINED POLLY_CLANG_CXX20_CMAKE_)
   return()
 else()
-  set(POLLY_CLANG_CXX17_CMAKE_ 1)
+  set(POLLY_CLANG_CXX20_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
 polly_init(
-    "clang / c++17 support"
+    "clang / c++20 support"
     "Ninja"
 )
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx20.cmake")
