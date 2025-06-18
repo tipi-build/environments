7c6ef381540a161e08d7597aae39d47f14d78f44
Damien Buhl (alias daminetreg) <damien@tipi.build>
2025-06-18 23:32:34 +0000

    :gear: tag official environmments by git revision
    With {{cmake_re_source_hash}} the official environments are refered to with a tag matching the commit id of cmake-re, matching the version of cmake-re which is used to build the official environment, impacting the envzip hash with the cmake-re original git revision.
    
    Therefore ensuring rebuilds of environments on new cmake-re releases.
    
    This replaces the use of {{tipi_version_hash}} which referred to the released version of cmake-re.

--- linux-ubuntu-1604.pkr.js/linux-ubuntu-1604.pkr.js
+++ linux-ubuntu-1604.pkr.js/linux-ubuntu-1604.pkr.js
@@ -3,7 +3,7 @@
   "builders": [
     {
       "type": "docker",
-      "image": "tipibuild/tipi-ubuntu-1604:{{tipi_cli_local_version}}",
+      "image": "tipibuild/tipi-ubuntu-1604:{{cmake_re_source_hash}}",
       "commit": true
     }
   ],
@@ -14,7 +14,4 @@
       "tag": "latest"
     }
   ]
-  ,"_tipi_version":"{{tipi_version_hash}}"
-
-
 }
