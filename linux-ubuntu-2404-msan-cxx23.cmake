48c88475e98b4afa5aab1a939e8f209c1bee4942
Damien Buhl (alias daminetreg) <damien@tipi.build>
2025-09-16 18:01:30 +0000

    :recycle: working linux-ubuntu-2404-msan-cxx23.cmake toolchain
    Factored out sanitizers options and removed ignorelist selection from
    toolchain, this is a project matter.

--- linux-ubuntu-2404-msan-cxx23.cmake
+++ linux-ubuntu-2404-msan-cxx23.cmake
@@ -1,9 +1,10 @@
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
 polly_init(
-    "clang 20 / MSAN"
+    "clang 20 / C++ 23 MSAN"
     "Ninja"
 )
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx23.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/sanitizers/msan.cmake")
