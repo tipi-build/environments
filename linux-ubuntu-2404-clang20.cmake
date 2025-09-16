de04c1069d576141422236a1b96f133e0b275a45
Damien Buhl (alias daminetreg) <damien@tipi.build>
2025-09-16 23:28:43 +0000

    :new: linux-ubuntu-2404-clang20 uninstrumented and -msan with C++23 mode

--- linux-ubuntu-2404-clang20.cmake
+++ linux-ubuntu-2404-clang20.cmake
@@ -1,9 +1,9 @@
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
 polly_init(
-    "clang 20 / MSAN"
+    "clang 20 / C++ 23"
     "Ninja"
 )
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/sanitizers/msan.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx23.cmake")
--- linux-ubuntu-2404-clang20.pkr.js/linux-ubuntu-2404-clang20.pkr.js
+++ linux-ubuntu-2404-clang20.pkr.js/linux-ubuntu-2404-clang20.pkr.js
@@ -3,7 +3,7 @@
   "builders": [
     {
       "type": "docker",
-      "image": "tipibuild/tipi-ubuntu-2404-msan:{{cmake_re_source_hash}}",
+      "image": "tipibuild/tipi-ubuntu-2404-clang20:{{cmake_re_source_hash}}",
       "commit": true
     }
   ]
