569718e49932808c7157049d8aba557d5a8c5fe4
Lambourl <luc@tipi.build>
2025-10-01 15:09:46 +0200

    🔧 use lld to link

--- linux-ubuntu-2404-static.cmake
+++ linux-ubuntu-2404-static.cmake
@@ -16,10 +16,11 @@ include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
 
 set(CMAKE_C_FLAGS   "-static")
 set(CMAKE_CXX_FLAGS "-static -nostdlib++ -stdlib=libc++")
-set(CMAKE_C_LINK_FLAGS   "-static -static-libgcc")
-set(CMAKE_CXX_LINK_FLAGS "-static -static-libgcc -nostdlib++ -stdlib=libc++  -static-libstdc++ -static-libgcc ")
+set(CMAKE_C_LINK_FLAGS   "-fuse-ld=lld -static -static-libgcc")
+set(CMAKE_CXX_LINK_FLAGS "-fuse-ld=lld -static -static-libgcc -nostdlib++ -stdlib=libc++  -static-libstdc++ -static-libgcc ")
 
 set(CMAKE_C_STANDARD_LIBRARIES   "${CMAKE_C_STANDARD_LIBRARIES} ${CMAKE_DL_LIBS}")
 set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_CXX_STANDARD_LIBRARIES} ${CMAKE_DL_LIBS} /usr/local/share/.tipi/clang/4f846ee/lib/libc++.a /usr/local/share/.tipi/clang/4f846ee/lib/libc++abi.a")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=lld")
 
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
